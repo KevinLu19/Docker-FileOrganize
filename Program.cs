99cd1bfd76e612351dcb80e569e8d7dc1a8e9649
Kevin Lu <kevinxz01@gmail.com>
2024-11-19 18:30:04 -0600

    feat[file_dir]: added cd to test dir

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+/*
+Project Overscope:
+The Docker File Organizer is a C# application designed to help users organize and manage files inside a 
+Docker container. The tool automates the process of categorizing, moving, or deleting files based on 
+specific rules, file types, or metadata.
+
+Key features:
+1. File Organizer:
+    Sort files into categories like Images, src, logs, etc.
+    Allow for custom categories if need be
+2. Rule management
+    Delete temp files or logs based on age / type
+
+3. Logging and monitoring
+    Maintain logs of files
+*/
---
+++ src/Categories/FileOrganizers.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+
+namespace src.Categories;
+
+public class FileOrganizers
+{
+    // Test directory (parent directory)
+    private string _test_dir_path = "/home/kevin/test";
+    public FileOrganizers()
+    {
+        try
+        {
+            Directory.SetCurrentDirectory(_test_dir_path);
+            Console.WriteLine("Current Directory Set to: ", _test_dir_path);
+        }
+        catch (DirectoryNotFoundException e)
+        {
+             Console.WriteLine(e.Message);
+        }
+    }
+}
