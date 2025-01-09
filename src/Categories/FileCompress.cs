ecf3913d4a83bab9d28eb350bd4282d48fc34022
Kevin Lu <kevinxz01@gmail.com>
2025-01-09 12:58:03 -0600

    refactor[file_comp]: removed old logic. Need to test new logic. Might need to put into try - catch.

--- src/Categories/FileCompress.cs
+++ src/Categories/FileCompress.cs
@@ -17,39 +17,6 @@ public class FileCompress : IFile
                 - This requires SharpZipLib.
         */
         
-        // Check if given path exists.
-        if (Directory.Exists(path))
-        {
-            /*
-                Want to extract the directory and place it in the sub-parent directory.
-                Ex:
-                ~/file_org/src/test/path
-
-                ~/file_org/archive => Location for all archived files.
-
-                Compressed + zipped path = zip_path:
-                ~/file_org/src/test
-
-                extract_path: Create a new folder and insert files inside of that folder.
-                Check if folder content doesn't already exists.
-            */
-            var archive_dir = "/app/archive";
-
-            // Check if archive dir exists (should not exist at first. Will need to transfer in a dir into docker)
-            // Create directory if it doesn't exist.
-            if (archive_dir != null)
-            {
-                Console.WriteLine("==============");
-                ZipFile.CreateFromDirectory(path, _ARCHIVE_DIR);
-                Console.WriteLine($"Created zip to path of: {_ARCHIVE_DIR}");
-                Console.WriteLine("==============");
-            }
-        }
-        else
-        {
-            Console.WriteLine($"{path} does not exist or cannot open. Take a look into that.");
-        }
-
         // Check if archive dir exists (it should be)
         if (!Directory.Exists(_ARCHIVE_DIR))
         {
